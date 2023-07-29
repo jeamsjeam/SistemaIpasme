2df20cb719aae48ef722e62e0dbd6c392ef48e9b
JCLov <joseclaudioloverajaimes@gmail.com>
2023-07-29 11:56:19 -0400

    Cambio de nombre al rol 'administrador'

--- Backend/src/database/data.sql
+++ Backend/src/database/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO ROL (nombre) VALUES ('administrador'),('Estandar');
+INSERT INTO ROL (nombre) VALUES ('Administrador'),('Estandar');
 INSERT INTO USUARIO (usuario, clave, nombre, rol_id) VALUES ('admin', 'admin', 'System',1);
 INSERT INTO GENERO (nombre) VALUES ('Masculino'),('Femenino');
 --INSERT INTO DEPENDENCIA (nombre) VALUES ('');
